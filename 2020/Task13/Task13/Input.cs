10d2980a6420900dfccf62e569e6e65d2600cad5
Angel <angel.luis.leal.baron@gmail.com>
2020-12-26 14:22:25 +0100

    Added Task 13.

---
+++ 2020/Task13/Task13/Input.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task13
+{
+    /// <summary>
+    /// Input in this task
+    /// </summary>
+    class Input
+    {
+        /// <summary>
+        /// Input id
+        /// </summary>
+        public String Id = String.Empty;
+        
+        /// <summary>
+        /// Earliest Time Stamp
+        /// </summary>
+        public int EarliestTimeStamp =0;
+
+        /// <summary>
+        /// Raw scheduled
+        /// </summary>
+        public List<String> Schedule = new List<String>();
+
+        /// <summary>
+        /// Dictionary which contains bus id's and their
+        /// offset with the first depature
+        /// </summary>
+        public Dictionary<int, int> Modules = new Dictionary<int, int>();
+
+        /// <summary>
+        /// Fill Modules Dictionary
+        /// </summary>
+        public void FillModules()
+        {
+
+            int counter = 0;
+
+            for (int i = 0; i < Schedule.Count; i++)
+            {
+                if (!Schedule[i].Equals("x"))
+                {
+                    int number = int.Parse(Schedule[i]);
+                    Modules.Add(number, (number-counter)%number);
+                }
+                    counter++;
+            }
+
+        }
+
+        /// <summary>
+        /// Calculates minimum waiting time
+        /// </summary>
+        /// <returns>Minimum waiting time</returns>
+        public int CalculateMinimumWaitingTime()
+        {
+            return (from m in Modules
+                    from n in Enumerable.Range(0, 100000)
+                    where (m.Key * n >= EarliestTimeStamp)
+                    orderby ((m.Key * n) - EarliestTimeStamp)
+                     select ((m.Key * n) - EarliestTimeStamp)*m.Key).First();
+        }
+
+        /// <summary>
+        /// Calculates Chinese RemainderTheorem (For Part 2)
+        /// </summary>
+        /// <returns>Chinese Remainder</returns>
+        public Int64 CalculateChineseRemainderTheorem()
+        {
+
+            Int64 leastCommonMultiple = 1;
+
+            foreach (int number in Modules.Keys)
+            {
+                leastCommonMultiple *= number;
+            }
+
+            Int64 acc = 0;
+
+            foreach (KeyValuePair<int, int> kvp in Modules)
+            {
+                Int64 division = leastCommonMultiple / kvp.Key;
+                acc += kvp.Value * ModularMultiplicativeInverse(division, kvp.Key) * division;
+            }
+
+            return (acc % leastCommonMultiple);
+
+        }
+
+        /// <summary>
+        /// Modular Multiplicative Reverse
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="mod">Module</param>
+        /// <returns>Result</returns>
+        private static Int64 ModularMultiplicativeInverse(Int64 value, Int64 mod)
+        {
+            Int64 module = value % mod;
+
+            for (int i = 1; i < mod; i++)
+            {
+                if ((module * i) % mod == 1)
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+
+    }
+}
---
+++ 2020/Task13/Task13/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task13
+{
+    class Program
+    {
+
+        /// <summary>
+        /// First Part
+        /// </summary>
+        static void FirstPart(Input input)
+        {
+            Console.WriteLine("First solution: {0}", input.CalculateMinimumWaitingTime());
+        }
+
+        /// <summary>
+        /// Second part
+        /// </summary>
+        static void SecondPart(Input input)
+        {
+            Console.WriteLine("Second solution: {0}", input.CalculateChineseRemainderTheorem() );
+        }
+
+        static void Main()
+        {
+
+            List<Input> inputs = new List<Input>
+            {
+                new Input()
+                {
+                    Id = "1",
+                    EarliestTimeStamp = 939,
+                    Schedule = { "7", "13", "x", "x", "59", "x", "31", "19" }
+                },
+
+                new Input()
+                {
+                    Id = "2",
+                    EarliestTimeStamp = 1002462,
+                    Schedule = { "37", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+                                "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+                                "x", "x", "x", "41", "x", "x", "x", "x", "x", "x", "x", "x",
+                                "x", "601", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+                                "x", "19", "x", "x", "x", "x", "17", "x", "x", "x", "x", "x",
+                                "23", "x", "x", "x", "x", "x", "29", "x", "443", "x", "x", "x",
+                                "x", "x", "x", "x", "x", "x", "x", "x", "x", "13" }
+                }
+            };
+
+            foreach (Input input in inputs)
+            {
+                Console.WriteLine("Testing Input {0}", input.Id);
+                Console.WriteLine();
+                Console.WriteLine();
+
+                input.FillModules();
+
+                FirstPart(input);
+                SecondPart(input);
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
