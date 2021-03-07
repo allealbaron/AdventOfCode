4974ed11fa9683fa75df69e1f3befd342804658e
Angel <angel.luis.leal.baron@gmail.com>
2021-03-07 19:40:33 +0100

    2019: Added Tasks 01 and 02.

---
+++ 2019/Task01/Task01/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task01
+{
+    class Program
+    {
+
+        /// <summary>
+        /// List of entiries
+        /// </summary>
+        static List<int> Entries = new List<int>();
+
+        /// <summary>
+        /// Calculates fuel necessary for the first part
+        /// </summary>
+        /// <param name="mass">Mass</param>
+        /// <returns>Fuel necessary</returns>
+        static int CalculateFuelFirstPart(int mass)
+        {
+            return int.Parse(Math.Floor((decimal)(mass / 3)).ToString()) -2;
+        }
+
+        /// <summary>
+        /// Calculates fuel necessary for the second part
+        /// </summary>
+        /// <param name="mass">Mass</param>
+        /// <returns>Fuel necessary</returns>
+        static int CalculateFuelSecondPart(int mass)
+        {
+
+            int result = 0;
+
+            while (mass > 2)
+            {
+                
+                int nextValue = CalculateFuelFirstPart(mass);
+
+                if (nextValue > 0)
+                {
+                    result += nextValue;
+                }
+                
+                mass = nextValue;
+
+            }
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// First Part
+        /// </summary>
+        static void FirstPart()
+        {
+
+            Console.WriteLine("First solution:");
+            Console.WriteLine("Necessary fuel: {0}", (from t in Entries select CalculateFuelFirstPart(t)).Sum());
+
+        }
+
+        /// <summary>
+        /// Second part
+        /// </summary>
+        static void SecondPart()
+        {
+
+            Console.WriteLine("Second solution:");
+            Console.WriteLine("Necessary fuel: {0}", (from t in Entries select CalculateFuelSecondPart(t)).Sum());
+
+        }
+
+        /// <summary>
+        /// Loads file
+        /// </summary>
+        /// <param name="fileName">File name</param>
+        static void LoadFile(string fileName)
+        {
+
+            Entries = new List<int>();
+
+            const Int32 BufferSize = 128;
+            FileStream fs = File.OpenRead(fileName);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8, true, BufferSize);
+            String line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Entries.Add(int.Parse(line));
+            }
+
+            sr.Close();
+            fs.Close();
+
+        }
+
+        /// <summary>
+        /// Main Thread
+        /// </summary>
+        static void Main()
+        {
+            List<string> files = new List<string>() { "TestInput.txt" , "Input.txt" };
+
+            foreach (string file in files)
+            {
+                Console.WriteLine("Testing file {0}", file);
+                Console.WriteLine();
+                Console.WriteLine();
+
+                LoadFile(file);
+                FirstPart();
+                SecondPart();
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ 2019/Task02/Task02/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task02
+{
+    class Program
+    {
+
+        /// <summary>
+        /// List of entiries
+        /// </summary>
+        static List<int> Entries = new List<int>();
+
+
+        /// <summary>
+        /// Computes First Part
+        /// </summary>
+        /// <returns>Value stored at position 0</returns>
+        static int ComputeFirstPart()
+        {
+
+            int i = 0;
+
+            while (Entries[i] != 99)
+            {
+                switch (Entries[i])
+                {
+                    case 1:
+                        Entries[Entries[i + 3]] = (Entries[Entries[i + +1]] + Entries[Entries[i + 2]]);
+                        i += 4;
+                        break;
+                    case 2:
+                        Entries[Entries[i + 3]] = (Entries[Entries[i + +1]] * Entries[Entries[i + 2]]);
+                        i += 4;
+                        break;
+                }
+            }
+
+            return Entries[0];
+
+        }
+
+        /// <summary>
+        /// Computes Second Part
+        /// </summary>
+        /// <returns>Result</returns>
+        static int ComputeSecondPart()
+        {
+            const int GOAL = 19690720;
+
+            int result = 0;
+            int noun = 0;
+            int verb = 0;
+
+            while (result != GOAL && verb<100)
+            {
+                LoadFile("Input.txt");
+
+                Entries[1] = noun;
+                Entries[2] = verb;
+
+                result = ComputeFirstPart();
+
+                if (result != GOAL)
+                {
+                    if (noun < 99)
+                    {
+                        noun++;
+                    }
+                    else
+                    {
+                        noun = 0;
+                        verb++;
+                    }
+                }
+            }
+
+            return (noun * 100 + verb);
+
+        }
+       
+        /// <summary>
+        /// First Part
+        /// </summary>
+        static void FirstPart()
+        {
+
+            Console.WriteLine("First solution:");
+            Console.WriteLine("Value at position 0: {0}", ComputeFirstPart());
+
+        }
+
+        /// <summary>
+        /// Second part
+        /// </summary>
+        static void SecondPart()
+        {
+
+            Console.WriteLine("Second solution:");
+            Console.WriteLine("Answer: {0}", ComputeSecondPart());
+
+        }
+
+        /// <summary>
+        /// Loads file
+        /// </summary>
+        /// <param name="fileName">File name</param>
+        static void LoadFile(string fileName)
+        {
+
+            Entries = new List<int>();
+
+            const Int32 BufferSize = 128;
+            FileStream fs = File.OpenRead(fileName);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8, true, BufferSize);
+            String line;
+
+            line = sr.ReadLine();
+
+            foreach( string s in line.Split(","))
+            { 
+                Entries.Add(int.Parse(s));
+            }
+
+            sr.Close();
+            fs.Close();
+
+        }
+
+        /// <summary>
+        /// Main Thread
+        /// </summary>
+        static void Main()
+        {
+            List<string> files = new List<string>() { "TestInput.txt" , "TestInput2.txt", "TestInput3.txt", 
+                                                       "TestInput4.txt", "TestInput5.txt", "Input.txt" };
+
+            foreach (string file in files)
+            {
+                Console.WriteLine("Testing file {0}", file);
+                Console.WriteLine();
+                Console.WriteLine();
+
+                LoadFile(file);
+
+                if (file == "Input.txt")
+                {
+                    Entries[1] = 12;
+                    Entries[2] = 2;
+                }
+
+                FirstPart();
+
+                if (file == "Input.txt")
+                {
+                    SecondPart();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
