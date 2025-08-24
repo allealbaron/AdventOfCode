3eb5aa094b95a7c6fb2d2adac5b746d02ae1b7c5
Ángel <anlba@eg.dk>
2025-08-24 12:58:28 +0200

    Add Task01 solution and tests for Advent of Code 2024
    Added the initial implementation for Task01, including the main program logic, project files, and unit tests. Updated the Readme to link to the new 2024 Task01. Included a .gitignore for Visual Studio and .NET projects.

---
+++ 2024/Task01/Task01/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace Task01;
+
+public class Program
+{
+
+    private readonly List<int> _leftList;
+    private readonly List<int> _rightList;
+
+    /// <summary>
+    /// Loads file
+    /// </summary>
+    /// <param name="fileName">File name</param>
+    private void LoadFile(string fileName)
+    {
+        const int bufferSize = 128;
+
+        using var fs = File.OpenRead(fileName);
+        using var sr = new StreamReader(fs, Encoding.UTF8, true, bufferSize);
+
+        while (sr.ReadLine() is { } line)
+        {
+            var splitValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            _leftList.Add(int.Parse(splitValues[0]));
+            _rightList.Add(int.Parse(splitValues[1]));
+        }
+
+        _leftList.Sort();
+        _rightList.Sort();
+
+    }
+
+    public int CalculateTotalDistance()
+    {
+        return _leftList.Select((t, i) => Math.Abs(t - _rightList[i])).Sum();
+    }
+
+    public List<(T Value, int Amount)> CountOccurrences<T>(List<T> input)
+    {
+
+        return input
+            .GroupBy(x => x)
+            .Select(g => (Value: g.Key, Amount: g.Count()))
+            .ToList();
+    }
+
+    public int CalculateSimilarity()
+    {
+
+        var rightCounts = CountOccurrences(_rightList)
+            .ToDictionary(x => x.Value, x => x.Amount);
+
+        return CountOccurrences(_leftList)
+            .Where(itemLeft => rightCounts.ContainsKey(itemLeft.Value))
+            .Sum(itemLeft => itemLeft.Amount * itemLeft.Value * rightCounts[itemLeft.Value]);
+
+    }
+
+    /// <summary>
+    /// Class creator
+    /// </summary>
+    /// <param name="fileName">File to load</param>
+    public Program(string fileName)
+    {
+
+        _leftList = [];
+        _rightList = [];
+        LoadFile(fileName);
+    }
+
+    /// <summary>
+    /// Main Thread
+    /// </summary>
+    private static void Main()
+    {
+        Program t = new(@"C:\Personal\AdventOfCode\2024\Input\Input.txt");
+
+        Console.WriteLine("First Part: {0}", t.CalculateTotalDistance());
+        Console.WriteLine("Second Part: {0}", t.CalculateSimilarity());
+        Console.ReadLine();
+    }
+
+}
---
+++ 2024/Task01/TestProjectTask01/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Task01;
+
+namespace TestProjectTask01;
+
+    public class Tests
+    {
+        private const string TestFile = @"C:\Personal\AdventOfCode\2024\Input\Test.txt";
+        private const string InputFile = @"C:\Personal\AdventOfCode\2024\Input\Input.txt";
+
+    [Test]
+        public void Test1Part1()
+        {
+
+            Program t = new(TestFile);
+
+            Assert.That(t.CalculateTotalDistance(), Is.EqualTo(11));
+        
+        }
+
+        [Test]
+        public void Test2Part1()
+        {
+
+            Program t = new(InputFile);
+
+            Assert.That(t.CalculateTotalDistance(), Is.EqualTo(2430334));
+
+        }
+
+        [Test]
+        public void Test1Part2()
+        {
+            Program t = new(TestFile);
+
+            Assert.That(t.CalculateSimilarity(), Is.EqualTo(31));
+
+        }
+
+        [Test]
+        public void Test2Part2()
+        {
+            Program t = new(InputFile);
+
+            Assert.That(t.CalculateSimilarity(), Is.EqualTo(28786472));
+
+        }
+
+}
